712b7848670d9835d30f348970a30305fd35a37c
macmaro <mateminfo@gmail.com>
2022-01-27 14:02:22 +0100

    making components

--- src/Components/PersonalSite/PersonalSite.js
+++ src/Components/PersonalSite/PersonalSite.js
@@ -7,18 +7,18 @@ export default function PersonalSite() {
     return(
 
         <div className = "flexbox-container">
-           <div className = "flexbox-item left-up-menu">
-                <Menu/>
+           <div className = "flexbox-item left-up-menu">   
+                <Menu title='First job'/>
             </div>        
                 
-                <div className = "flexbox-item intro">
-                    Welcome on Ada Marosz site!
-                </div>
-                <div className = "flexbox-item image">
-                    <img className = "flexbox-item img" src = {imageSrc} alt="Ada"/>
-                </div>
-                <div className ="flexbox-item portfolio">portfolio portfolio</div>
-                <button className = "flexbox-item menu" >MENU</button>
+            <div className = "flexbox-item intro">
+                Welcome on Ada Marosz site!
+            </div>
+            <div className = "flexbox-item image">
+                <img className = "flexbox-item img" src = {imageSrc} alt="Ada"/>
+            </div>
+            <div className ="flexbox-item portfolio">portfolio portfolio</div>
+            <button className = "flexbox-item menu" >MENU</button>
           
             
         </div>
